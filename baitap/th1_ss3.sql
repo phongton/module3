337d93cedf6912746eeb966fed16ab48c3e28467
Phong Tôn <tonthathoangphong@MacBook-Air-cua-Ton.local>
2024-06-19 08:26:06 +0700

    done

---
+++ baitap/th1_ss3.sql
@@ -0,0 +1,17 @@
+use QuanlySinhVien;
+insert into Class
+values(1,'A1','2008-12-20',1),
+(2,'A2','2008-11-20', 0),
+(3,'A3','2009-10-20',1);
+insert into Student
+values(1,'hung','ha noi',0901234561,1,1),
+(2,'hoa' ,'hai phong',0909090909,1,2),
+(3,'manh','da nang',0980808080,0,3);
+insert into subject
+values(1,'dadsd',1,1),
+(2,'aqweq',3,1),
+(3,'weqeq',2,1);
+insert into Mark
+values(1,1,1,8,1),
+(2,1,2,10,2),
+(3,2,1,12,1);
